8df7b97e19b4b47d460cb3f6deca9ed3e5e76298
GitHub Action <action@github.com>
2024-09-21 09:47:13 +0000

    Deploy updated dist folder

---
+++ dist/d2d.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name          D2D
+// @version       2024.9.21
+// @namespace     https://github.com/towiess/d2d
+// @author        prljav
+// @description   D2D towies extension
+// @homepageURL   https://github.com/towiess/d2d#d2d
+// @downloadURL   https://raw.github.com/towiess/d2d/main/dist/d2d.user.js
+// @updateURL     https://raw.github.com/towiess/d2d/main/dist/d2d.user.js
+// @supportURL    https://github.com/towiess/d2d/issues
+// @grant         none
+// @match         https://drednot.io/*
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	console.log('lol');
+
+})();
